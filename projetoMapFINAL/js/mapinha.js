3b4349814a36f7ce1e1da0cde44161e2ed9c8b44
kauaas <57207681+kauaas@users.noreply.github.com>
2019-11-04 00:52:57 -0300

    Projeto finalizado.
    projeto finalizado com algumas alterações, e passagem de parametros...

---
+++ projetoMapFINAL/js/mapinha.js
@@ -0,0 +1,91 @@
+var resultado;
+
+function valorCidade(){
+    var city = document.getElementById('cidade').value;
+    return city;
+}
+
+
+function requisi() {
+	//fazendo a requisição
+	var requisicao = new XMLHttpRequest();
+	var url = "http://api.mapbox.com/geocoding/v5/mapbox.places/"+ valorCidade() +".json?access_token=pk.eyJ1IjoibGF1cmluZWEiLCJhIjoiY2sxaHI2M3J2MWk3bjNncW93a2ZneTIyMSJ9.IsCqY34SiFRNoGtHLTnEtQ"
+    requisicao.open('GET', url, true); //abrindo a conexão
+console.log(url);
+    requisicao.onreadystatechange = function(e) {
+    	if (this.readyState == 4) {
+    	//	consnova_ole.log(JSON.parse(this.response)); 
+    		resultado = JSON.parse(this.response);
+            for(let i = 0; i<5; i++){
+                inserirItem(resultado.features[i].place_name, i, resultado.features[i].geometry.coordinates[1], resultado.features[i].geometry.coordinates[0]);
+              }
+        }
+        
+    }
+    requisicao.send();
+    
+}
+
+function elementoOl(){
+document.getElementById('listinha').innerHTML = " ";
+	var nova_div = document.createElement('div');
+	nova_div.setAttribute('id', 'nova_div');
+	var nova_ol = document.createElement('ol');
+	nova_ol.setAttribute('id', 'nova_ol');
+	document.getElementById('listinha').appendChild(nova_div);
+	document.getElementById('nova_div').appendChild(nova_ol);
+	
+}
+
+function inserirItem(nome_do_item, id, lat, long){
+        
+    var arm = 'mostrarMapa('+lat+','+long+')';
+    var nova_li = document.createElement('li');
+    nova_li.setAttribute('id', id);
+    var nova_link = document.createElement('a');
+    
+    nova_link.setAttribute('onclick', arm);
+    var texto = document.createTextNode(nome_do_item);
+    nova_link.appendChild(texto);
+    nova_li.appendChild(nova_link); 
+    document.getElementById('nova_ol').appendChild(nova_li);
+    
+}
+
+
+function criarMap(){
+    var map = document.createElement('div');
+    map.setAttribute('id', "mapid");
+    map.style.height = "500px";
+    map.style.width = "500px";
+    document.getElementById('fixed').appendChild(map);
+}
+
+function mostrarMapa(lat, long){
+    document.getElementById('fixed').innerHTML = " ";
+    criarMap();
+
+    var mymap = L.map('mapid').setView([lat, long], 15);
+    
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2ZsYmVkdWNhdG9yIiwiYSI6ImNrMTZrYm1vNTA1dWEzaGxqN2tmMTZlazcifQ.XXsWkpgiguegb-C7WQpGBA', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+    }).addTo(mymap);
+    L.marker([lat, long]).addTo(mymap);
+    L.circle([lat, long], 100, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap).bindPopup("this is a circle! BIOS");
+
+}
+
+function main(){
+    elementoOl();
+    requisi();
+    
+
+}
